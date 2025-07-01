1cff39fafc4e3bb31f04c116a9c8b0c10bea686b
Airen Alires <alires_airen_kenne01@student.smc.edu>
2025-07-01 16:34:28 -0700

    Add remaining functions and reflection files

--- journal.js
+++ journal.js
@@ -1,4 +1,17 @@
 // Greet a user by name
 function greet(name) {
   return "Hello, " + name + "!";
+} function subtract(num1, num2) {
+    return num1 - num2;
+} function multiply (factor1, factor2) {
+    return factor1 * factor2;
+} function isPositive(value) {
+    return value > 0;
+} function getFirstName(fullName) {
+  return fullName.split(' ')[0];
+} function capitalize(word) {
+  if (word.length === 0) {
+    return "";
+  }
+  return word.charAt(0).toUpperCase() + word.slice(1);
 }
